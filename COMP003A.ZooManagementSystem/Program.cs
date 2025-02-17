72e87410fa2ce8842592e605969ced5f125d0e35
flores-97 <floresvictor15@gmail.com>
2025-02-16 21:54:23 -0800

    zoo utility and menu selector built

--- COMP003A.ZooManagementSystem/Program.cs
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -8,8 +8,58 @@ namespace COMP003A.ZooManagementSystem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Zoo Management System!");
+
+
+            int choice;
+            int done;
+
+            while (true)
+            {
+                Console.WriteLine("\nInventory Management System Menu: ");
+                Console.WriteLine("1. Add a Lion");
+                Console.WriteLine("2. Add a Parrot");
+                Console.WriteLine("3. Display All Animals");
+                Console.WriteLine("4. Describe an Animal");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice: ");
+
+                choice = int.Parse(Console.ReadLine());
+
+
+
+                if (choice == 5) break;
+
+                else if (choice == 1)
+                {
+
+                }
+
+                else if (choice == 2)
+                {
+
+                }
+
+
+                else if (choice == 3)
+                {
+
+                }
+
+                else if (choice == 4)
+                {
+
+                }
+
+                else
+                {
+                    Console.WriteLine("\nInvalid entry. Select an option on the menu.\n");
+                }
+            }
 
         }
-        class ZooUtility
+    }
+
     }
 }
---
+++ COMP003A.ZooManagementSystem/ZooUtility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class ZooUtility
+    {
+        /// <summary>
+        /// display animal name 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string DescribeAnimal(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        /// <summary>
+        /// display animal name and species
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="species"></param>
+        /// <returns></returns>
+        public string DescribeAnimal(string name, string species)
+        {
+            Console.WriteLine(name, species);
+        }
+
+        /// <summary>
+        /// display name, species, and age
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="species"></param>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        public string DescribeAnimal(string name, string species, int age)
+        {
+            Console.WriteLine(name, species, age);
+        }
+    }
+}
